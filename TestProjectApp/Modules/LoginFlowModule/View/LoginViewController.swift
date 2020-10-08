1f9a330cd2f90fbcfb26475aba8c1506c747d25b
Roman Suvorov <kikiwora@icloud.com>
2020-10-08 11:27:21 +0300

    Add LoginForm scroll for keyboard accomodation

--- TestProjectApp/Modules/LoginFlowModule/View/LoginViewController.swift
+++ TestProjectApp/Modules/LoginFlowModule/View/LoginViewController.swift
@@ -9,18 +9,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    var presenter: LoginViewOutput! {
+        didSet {
+            (presenter as! LoginFlowPresenter).loginView = self
+        }
+    }
+
     @IBOutlet weak var loginFormViewContainer: UIView! {
         didSet {
             loginFormView = LoginFormView.instantiateFromNib(in: loginFormViewContainer)
             loginFormView.onLogin = { [weak self] in self?.loginCallback?($0) }
         }
     }
-
-    var presenter: LoginViewOutput! {
-        didSet {
-            (presenter as! LoginFlowPresenter).loginView = self
-        }
-    }
+    @IBOutlet weak var loginScrollView: UIScrollView!
+    @IBOutlet weak var loginScrollViewBottomMargin: NSLayoutConstraint!
 
     var loginFormView: LoginFormView!
     var loginCallback: LoginFormView.LoginCallback?
@@ -28,8 +30,44 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLoginCallback()
+        setupKeyboardEvents()
+    }
+}
+
+private extension LoginViewController {
+
+    func setupKeyboardEvents() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+
         hideKeyboardWhenTappedAround()
         loginFormView.onKeyboardDismiss = { [weak self] in self?.dismissKeyboard() }
+    }
+
+    func adjustInsetForKeyboardShow(_ show: Bool, notification: NSNotification) {
+        guard let userInfo = notification.userInfo, let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            let adjustmentHeight = keyboardFrame.cgRectValue.height
+            self?.loginScrollViewBottomMargin.constant = show ? adjustmentHeight : 0
+            self?.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        adjustInsetForKeyboardShow(true, notification: notification)
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        adjustInsetForKeyboardShow(false, notification: notification)
     }
 }
 
